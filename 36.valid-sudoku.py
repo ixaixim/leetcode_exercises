c296faede807a7bda96b6dd2aa309602c1d023ea
Neel Misciasci <neel.miscia@gmail.com>
2023-09-11 09:49:31 +0200

    exe: valid sudoku

---
+++ 36.valid-sudoku.py
@@ -0,0 +1,40 @@
+# @before-stub-for-debug-begin
+from python3problem36 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+from collections import defaultdict
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # this problem requires O(n^2) time complexity.
+        # However, there are a few tricks that can speed up the computation. 
+        # Here we use a dictionary of sets, one dictionary for each column/row/box. 
+        box_number = lambda r,c: 3*(r//3) + (c//3) # r row number, c col number
+        hash_map_box = defaultdict(set)
+        hash_map_col = defaultdict(set)
+        hash_map_row = defaultdict(set)
+
+        for r in range(9):
+            for c, val in enumerate(board[r]):
+                if val == '.':
+                    continue
+                val = int(val)
+                box_index = box_number(r,c)
+                if (
+                    (val in hash_map_box[box_index]) or 
+                    (val in hash_map_col[c]) or 
+                    (val in hash_map_row[r])):
+                    return False
+                hash_map_box[box_index].add(val)
+                hash_map_col[c].add(val)
+                hash_map_row[r].add(val)
+
+        return True
+# @lc code=end
+
