8e881aa07f46404307076abc40cff9c4204d9248
Neel Misciasci <neel.miscia@gmail.com>
2023-10-20 18:20:11 +0200

    valid palindrome

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -7,6 +7,27 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+        # i = 0 
+        # j = len(s)-1
+        # while i < j:
+        #     while (not s[i].isalnum()) and i<j:
+        #         i +=1
+        #     while (not s[j].isalnum()) and i<j:
+        #         j -= 1
+        #     if s[i].lower() != s[j].lower():
+        #         return False
+        #     i +=1
+        #     j -=1
+        # return True
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum(): i += 1
+            while i < j and not s[j].isalnum(): j -= 1
+
+            if s[i].lower() != s[j].lower(): return False
+            i += 1
+            j -= 1
+
+        return True
 # @lc code=end
 
